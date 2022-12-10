497a08e4be91b401e19ab23874c583812965bf9c
Matt Senick <32623708+Matts52@users.noreply.github.com>
2022-12-10 01:29:38 -0600

    Add files via upload

---
+++ Glossary/Glossary.js
@@ -0,0 +1,23 @@
+
+
+
+window.onload = function () {
+
+
+    fetch('../Data/Glossary_Data.json')
+        .then(response => response.json())
+        .then(data => {
+
+            for (var i = 0; i < data.Sheet1.length; i++){
+                //console.log(data.Sheet1[i].Chapter);
+                var h3 = document.createElement("p");
+                h3.innerHTML = "<b>"+ data.Sheet1[i].Term +"</b>"
+                var p = document.createElement("p");
+                p.innerHTML = data.Sheet1[i].Interpretation;
+
+                document.getElementById(data.Sheet1[i].Chapter).appendChild(h3).appendChild(p);
+            }
+    });
+
+
+}
