dc7049fc36d7f061f41b8e33ab687d946170f5ad
Matt Senick <Matt.s5@hotmail.com>
2023-11-05 21:44:25 -0600

    Add a selectable video option mapping to the correct playable video

---
+++ Animations/Animations.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    // Define an object to map option values to video sources
+    var videoSources = {
+        "option1": "../Assets/ExpectedValue.mp4",
+        "option2": "../Assets/ContinuousExpectation.mp4",
+        "option3": "../Assets/ConditionalProbability.mp4",
+        "option4": "../Assets/DGPSampling.mp4",
+        "option5": "../Assets/BiasedEstimator.mp4"
+    };
+    
+    // get our elements
+    var selectBox = document.getElementById("selectBox");
+    var videoElement = document.querySelector("video");
+
+    // Add an event listener to the select box
+    selectBox.addEventListener("change", function () {
+        var selectedValue = selectBox.value;
+
+        // Check if the selected value exists in the video sources object
+        if (videoSources.hasOwnProperty(selectedValue)) {
+            videoElement.src = videoSources[selectedValue];
+            videoElement.load(); // Load the new video source
+        } else {
+            // Handle the case when the selected option doesn't have a corresponding video source
+            alert("No video source found for the selected option.");
+        }
+    });
+});
