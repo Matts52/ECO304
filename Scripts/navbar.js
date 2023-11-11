3bf3de2383017a47f33b9238c621b6254544a6e7
Matt Senick <Matt.s5@hotmail.com>
2023-11-11 13:06:04 -0600

    simplify navbar generation

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -5,21 +5,23 @@ function generateNavbar() {
  
    console.log(currentPage)
 
+   fixedLocation = '/ECO304'
+
     // Define the navigation links
     var navLinks = [
-       { id: 'home', text: 'Home', href: '/ECO304/' },
-       { id: 'questions', text: 'Questions', href: '/ECO304/Questions' },
-       { id: 'visualizations', text: 'Visualizations', href: '/ECO304/Visualizations' },
-       { id: 'animations', text: 'Animations', href: '/ECO304/Animations' },
-       { id: 'gradeCalc', text: 'Grade Calculator', href: '/ECO304/GradeCalc' },
-       { id: 'glossary', text: 'Glossary', href: '/ECO304/Glossary' }
+       { id: 'home', text: 'Home', href: '/' },
+       { id: 'questions', text: 'Questions', href: '/Questions' },
+       { id: 'visualizations', text: 'Visualizations', href: '/Visualizations' },
+       { id: 'animations', text: 'Animations', href: '/Animations' },
+       { id: 'gradeCalc', text: 'Grade Calculator', href: '/GradeCalc' },
+       { id: 'glossary', text: 'Glossary', href: '/Glossary' }
     ];
  
     // Create the navbar HTML
     var navbarHtml = '<div class="topnav">';
     for (var i = 0; i < navLinks.length; i++) {
        var link = navLinks[i];
-       navbarHtml += '<a id="' + link.id + '" href="' + link.href + '">' + link.text + '</a>';
+       navbarHtml += '<a id="' + link.id + '" href="' + fixedLocation + link.href + '">' + link.text + '</a>';
     }
     navbarHtml += '</div>';
  
