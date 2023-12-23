34a3067ebb7b0a016d83c09aaef9001cf26a3ddf
Matt Senick <Matt.s5@hotmail.com>
2023-12-23 15:51:17 -0600

    get a chart showing

--- HistoricalGrades/HistoricalGrades.js
+++ HistoricalGrades/HistoricalGrades.js
@@ -12,9 +12,50 @@ async function visualizeGrades() {
 
     console.table(grades_data);
 
+    // Initialize an array to store counts in each bucket
+    const bucketCounts = Array(20).fill(0);
 
+    // Calculate counts in each bucket
+    grades_data.forEach(entry => {
+      const grade = entry.Grade;
+      const bucketIndex = Math.floor(grade / 5); // Assuming 20 buckets, each of width 5
+      bucketCounts[bucketIndex]++;
+    });
 
+    const canvas = document.getElementById('histogramChart');
+    const ctx = canvas.getContext('2d');
 
+    // generate the chart
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: Array.from({ length: 20 }, (_, index) => `${index * 5}-${(index + 1) * 5 - 1}`),
+        datasets: [{
+          label: 'Grade Distribution',
+          data: bucketCounts,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          x: { 
+            title: {
+              display: true,
+              text: 'Grade Range'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Count'
+            }
+          }
+        }
+      }
+    });
 }
+
 
 
